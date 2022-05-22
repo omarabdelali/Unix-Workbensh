40d6b3f0e003a36faf6f5f4aaf60e4617d5bc85c
omarabdelali <omar.chorbani@gmail.com>
2022-05-22 17:30:13 +0100

    guessinggame.sh

---
+++ guessinggame.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+function guess(){
+    answer=$(ls -l |grep "^-"|wc -l)
+    while true;
+    do
+        echo "please enter your guess: "
+        read  number
+        if [ $number -lt $answer ]
+        then
+            echo "your guess is Less then the true number"
+        elif [ $number -gt $answer ]
+        then
+            echo "your guess is Greater then the true number"
+        else
+            echo " congratulation,you are right!"
+        break;
+        fi
+    done
+}
+echo "guess the files number in the current directory!"
+guess
